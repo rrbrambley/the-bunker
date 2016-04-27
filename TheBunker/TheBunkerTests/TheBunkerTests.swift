9aa92298cf06dc53f1fbea15cec48c1e35ce19f0
Rob Brambley <rrbrambley@gmail.com>
2016-04-27 09:10:34 -0700

    Add empty Xcode workspace with cocoapods init'd

---
+++ TheBunker/TheBunkerTests/TheBunkerTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheBunkerTests.swift
+//  TheBunkerTests
+//
+//  Created by Robert Brambley on 4/27/16.
+//  Copyright © 2016 AATT. All rights reserved.
+//
+
+import XCTest
+@testable import TheBunker
+
+class TheBunkerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
